9908d6e6fdb3c5344113f39b68519781f61519ef
KritikaTiwari <84006402+KritikaTiwari@users.noreply.github.com>
2022-07-29 21:12:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,8 +21,9 @@ def predict():
     exp1 = float(request.args.get('exp1'))
     exp2= float(request.args.get('exp2'))
     exp3= float(request.args.get('exp3'))
-   
-    prediction = model.predict([[exp,exp1,exp2,exp3]])
+    Bricks=float(request.args.get('Bricks'))
+    Bricks=float(request.args.get('neighbourhood'))
+    prediction = model.predict([[exp,exp1,exp2,exp3,Bricks,neighbourhood]])
 
     return render_template('show.html', prediction_text='Regression Model  has predicted price for the house : {}'.format(prediction))
   
