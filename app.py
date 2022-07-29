de6bb6d9c4ef3c564bfeec3c57dc0cbe553aed9f
KritikaTiwari <84006402+KritikaTiwari@users.noreply.github.com>
2022-07-29 14:22:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,3 +31,5 @@ def predict():
 
     return render_template('index.html', prediction_text='Regression Model  has predicted price for the house : {}'.format(prediction))
   
+if __name__ == "__main__":
+    app.run(debug=True)
